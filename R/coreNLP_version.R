5bbf9b1ee026485780b0acad45987c0bd7a3aaec
wcsherb <38059097+wcsherb@users.noreply.github.com>
2023-05-23 14:49:15 -0400

    Update coreNLP_version.R
    Added a note to update the version number.

--- R/coreNLP_version.R
+++ R/coreNLP_version.R
@@ -7,6 +7,8 @@
 #' @examples
 #' coreNLP_version
 # coreNLP_version <- 'stanford-corenlp-full-2017-06-09'
+
+# Don't forget to keep this updated with the latest version
 coreNLP_version <- 'stanford-corenlp-4.5.4'
 
 
